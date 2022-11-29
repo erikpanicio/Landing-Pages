34b61a325c151c49cc0af34c23a8d24fb7c35898
Erik Panicio <erikpanicio.dev@gmail.com>
2022-11-29 19:28:39 -0300

    botão que muda de cor

--- Cafe Street 2/script.js
+++ Cafe Street 2/script.js
@@ -9,53 +9,43 @@ function clicado() {
     }
 }
 
-function first() {
-    var box_review1 = document.getElementsByClassName("box-review1")[0]    
-    var box_review2 = document.getElementsByClassName("box-review2")[0]
-    var box_review3 = document.getElementsByClassName("box-review3")[0]
+var box_review1 = document.getElementsByClassName("box-review1")[0]    
+var box_review2 = document.getElementsByClassName("box-review2")[0]
+var box_review3 = document.getElementsByClassName("box-review3")[0]
 
+var review_button_first = document.getElementsByClassName("review-button first")[0]
+var review_button_second = document.getElementsByClassName("review-button second")[0]
+var review_button_third = document.getElementsByClassName("review-button third")[0]
+
+function first() {
     box_review1.style.display = "flex"
     box_review2.style.display = "none"
     box_review3.style.display = "none"
+
+    review_button_first.style.backgroundColor = "#7c5912"
+    review_button_second.style.backgroundColor = "#ffffff00"
+    review_button_third.style.backgroundColor = "#ffffff00"
+
 }
 
 function second() {
-    var box_review1 = document.getElementsByClassName("box-review1")[0]    
-    var box_review2 = document.getElementsByClassName("box-review2")[0]
-    var box_review3 = document.getElementsByClassName("box-review3")[0]
-
     box_review1.style.display = "none"
     box_review2.style.display = "flex"
     box_review3.style.display = "none"
 
+    review_button_first.style.backgroundColor = "#ffffff00"
+    review_button_second.style.backgroundColor = "#7c5912"
+    review_button_third.style.backgroundColor = "#ffffff00"
 
 }
 
 function third() {
-    var box_review1 = document.getElementsByClassName("box-review1")[0]    
-    var box_review2 = document.getElementsByClassName("box-review2")[0]
-    var box_review3 = document.getElementsByClassName("box-review3")[0]
-
     box_review1.style.display = "none"
     box_review2.style.display = "none"
     box_review3.style.display = "flex"
+
+    review_button_first.style.backgroundColor = "#ffffff00"
+    review_button_second.style.backgroundColor = "#ffffff00"
+    review_button_third.style.backgroundColor = "#7c5912"
+
 }
-
-/* 
-
-function first() {
-    var box_review = document.getElementsByClassName("box-review")[0]
-    box_review.style.marginLeft = "0px"
-}
-
-function second() {
-    var box_review = document.getElementsByClassName("box-review")[0]
-    box_review.style.marginLeft = "-1024.8px"
-}
-
-function third() {
-    var box_review = document.getElementsByClassName("box-review")[0]
-    box_review.style.marginLeft = "-2049.6px"
-}
-
-*/
