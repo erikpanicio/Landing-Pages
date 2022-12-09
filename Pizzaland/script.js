5c76412d4b51dcdfb7c01143d9c9e8b2680e6e60
Erik Panicio <erikpanicio.dev@gmail.com>
2022-12-09 19:44:45 -0300

    arrumando o menu

--- Pizzaland/script.js
+++ Pizzaland/script.js
@@ -3,9 +3,11 @@ var num = 0
 function clicked() {
     var header_center = document.getElementsByClassName("header-center")[0]
     header_center.style.display = "flex"
+    document.body.style.overflowY = "hidden"
     num += 1
     if(num > 1) {
         header_center.style.display = "none"
+        document.body.style.overflowY = "visible"
         num = 0
     }
 }
