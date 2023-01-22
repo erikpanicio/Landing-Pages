4515c622ad3003c05136c13def645b79566f19e0
Erik Panicio <erikpanicio.dev@gmail.com>
2023-01-21 22:05:22 -0300

    melhorias

--- Cafe Street/script.js
+++ Cafe Street/script.js
@@ -9,6 +9,8 @@ function clicado() {
     }
 }
 
+/* Comments */
+
 var box_review1 = document.getElementsByClassName("box-review1")[0]    
 var box_review2 = document.getElementsByClassName("box-review2")[0]
 var box_review3 = document.getElementsByClassName("box-review3")[0]
@@ -26,6 +28,10 @@ function first() {
     box_review2.style.display = "none"
     box_review3.style.display = "none"
 
+    box_review1.style.opacity = "100%"
+    box_review2.style.opacity = "0%"
+    box_review3.style.opacity = "0%"
+
     review_button_first.style.backgroundColor = "#7c5912"
     review_button_second.style.backgroundColor = "#ffffff00"
     review_button_third.style.backgroundColor = "#ffffff00"
@@ -36,6 +42,10 @@ function second() {
     box_review1.style.display = "none"
     box_review2.style.display = "flex"
     box_review3.style.display = "none"
+
+    box_review1.style.opacity = "0%"
+    box_review2.style.opacity = "100%"
+    box_review3.style.opacity = "0%"
 
     review_button_first.style.backgroundColor = "#ffffff00"
     review_button_second.style.backgroundColor = "#7c5912"
@@ -48,14 +58,39 @@ function third() {
     box_review2.style.display = "none"
     box_review3.style.display = "flex"
 
+    box_review1.style.opacity = "0%"
+    box_review2.style.opacity = "0%"
+    box_review3.style.opacity = "100%"
+
     review_button_first.style.backgroundColor = "#ffffff00"
     review_button_second.style.backgroundColor = "#ffffff00"
     review_button_third.style.backgroundColor = "#7c5912"
 
 }
 
-/* TESTE */
+/* Rotação automática do comments */
 
+var valor = review_button_first
+
+setInterval(rotacao, 5000)
+
+function rotacao() {
+    if (valor == review_button_first) {
+        first()
+        valor = review_button_second
+    }
+    else if (valor == review_button_second) {
+        second()
+        valor = review_button_third
+    }
+    else if (valor == review_button_third){
+        third()
+        valor = review_button_first
+    }
+}
+
+/* TESTE */
+/* 
 while (largura > "1175px"){
     var largura = screen.width
 }
@@ -64,4 +99,4 @@ if (largura <= "1175px"){
     box_review1.style.display = "none"
     box_review2.style.display = "none"
     box_review3.style.display = "none"
-}
+} */
